ad858c40248d06e445df024814f798a22440df18
xinzhilinger <2267292205@qq.com>
2021-12-04 11:26:14 +0800

    fsdf

---
+++ Assets/Scripts/BagDisplayUI.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BagDisplayUI : MonoBehaviour
+{
+    //单例模式
+    static BagDisplayUI bagDisplayUI;
+    private void Awake()
+    {
+        if(bagDisplayUI!=null)
+        {
+            Destroy(this);
+            
+        }
+        bagDisplayUI = this;
+               
+    }
+    private void OnEnable()
+    {
+        updateItemToUI();
+    }
+    //背包数据仓库、格子中物体预制体、和UI中显示物体元素的父元素
+    public MainItem mainItem;
+    public Grid gridPrefab;
+    public GameObject myBag; 
+
+    /// <summary>
+    /// 在UI中将一个物体的数据仓库显示出来
+    /// </summary>
+    /// <param name="item"></param>
+    public static void insertItemToUI(Item item)
+    {
+        
+        Grid grid = Instantiate(bagDisplayUI.gridPrefab, bagDisplayUI.myBag.transform);
+        grid.gridImage.sprite = item.itemImage;
+        grid.girdNum.text = item.itemNum.ToString();
+        
+    }
+
+    /// <summary>
+    /// 将背包数据仓库中所有物体显示在UI上
+    /// </summary>
+    public static void updateItemToUI()
+    {
+        for (int i = 0; i < bagDisplayUI.myBag.transform.childCount; i++)
+        {
+            Destroy(bagDisplayUI.myBag.transform.GetChild(i).gameObject);
+        }
+        for (int i = 0; i < bagDisplayUI.mainItem.itemList.Count; i++)
+        {
+            insertItemToUI(bagDisplayUI.mainItem.itemList[i]);
+        }
+    }
+}
---
+++ Assets/Scripts/BagScripts/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName ="New Item",menuName ="Bag/New Item")]
+public class Item : ScriptableObject
+{
+    //物体名、需要在UI中显示的图片、持有物体的数量、物体信息的描述
+    public string itemName;  
+    public Sprite itemImage; 
+    public int itemNum;      
+    [TextArea] //改变输入框格式，提示输入框容量
+    public string itemInfo;   
+}
